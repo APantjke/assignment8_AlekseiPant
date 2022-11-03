b06a766f7a48d86b9894b4b6cddbfeaa1a73c4db
Aleksei Pant <apantjke@ut.ee>
2022-11-03 19:33:10 +0200

    Добавьте файлы проекта.

---
+++ assignment8_AlekseiPant/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace assignment8_AlekseiPant
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.searchBtn = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.boysNameBox = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.girlsNameBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // searchBtn
+            // 
+            this.searchBtn.Location = new System.Drawing.Point(103, 150);
+            this.searchBtn.Name = "searchBtn";
+            this.searchBtn.Size = new System.Drawing.Size(75, 23);
+            this.searchBtn.TabIndex = 3;
+            this.searchBtn.Text = "Search";
+            this.searchBtn.UseVisualStyleBackColor = true;
+            this.searchBtn.Click += new System.EventHandler(this.searchButton);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Location = new System.Drawing.Point(103, 179);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(75, 23);
+            this.clearBtn.TabIndex = 4;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearButton);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.exitBtn.Location = new System.Drawing.Point(103, 228);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(75, 23);
+            this.exitBtn.TabIndex = 5;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitButton);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(100, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter a boy\'s name here:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(100, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(164, 18);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Enter a girls name here:";
+            // 
+            // boysNameBox
+            // 
+            this.boysNameBox.Location = new System.Drawing.Point(278, 103);
+            this.boysNameBox.Name = "boysNameBox";
+            this.boysNameBox.Size = new System.Drawing.Size(150, 20);
+            this.boysNameBox.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(278, 150);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(150, 51);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(278, 200);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(150, 51);
+            this.textBox2.TabIndex = 7;
+            // 
+            // girlsNameBox
+            // 
+            this.girlsNameBox.Location = new System.Drawing.Point(278, 53);
+            this.girlsNameBox.Name = "girlsNameBox";
+            this.girlsNameBox.Size = new System.Drawing.Size(150, 20);
+            this.girlsNameBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 281);
+            this.Controls.Add(this.girlsNameBox);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.boysNameBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.searchBtn);
+            this.Name = "Form1";
+            this.Text = "Name Search";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button searchBtn;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox boysNameBox;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox girlsNameBox;
+    }
+}
+
---
+++ assignment8_AlekseiPant/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace assignment8_AlekseiPant
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void searchButton(object sender, EventArgs e)
+        {
+            try
+            {
+                bool foundG = false, foundB = false;
+                string[] girlsNamesArray = new string[200];
+                string[] boysNamesArray = new string[200];
+                StreamReader girlsfile, boysfile;
+                girlsfile = File.OpenText("girls.txt");
+                boysfile = File.OpenText("boys.txt");
+                int girlsIndex = 0, boysIndex = 0;
+
+                while (!girlsfile.EndOfStream && !foundG)
+                {
+                    girlsIndex++;
+                    girlsNamesArray[girlsIndex] = girlsfile.ReadLine();
+
+                    if (girlsNamesArray[girlsIndex] == girlsNameBox.Text)
+                    {
+                        foundG = true;
+                        textBox1.Text = girlsNameBox.Text + ", your name is on the list" + "\r" + "\n" + "position: " + girlsIndex;
+                    }
+                    else
+                    {
+                        textBox1.Text = "Your name is not on the list";
+                    }
+                }
+                girlsfile.Close();
+
+                while (!boysfile.EndOfStream && !foundB)
+                {
+                    boysIndex++;
+                    boysNamesArray[boysIndex] = boysfile.ReadLine();
+
+                    if (boysNamesArray[boysIndex] == boysNameBox.Text)
+                    {
+                        foundB = true;
+                        textBox2.Text = boysNameBox.Text + ", your name is on the list" + "\r" + "\n" + "position: " + boysIndex;
+                    }
+                    else
+                    {
+                        textBox2.Text = "Your name is not on the list";
+                    }
+                }
+                boysfile.Close();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Name was not found");
+            }
+        }
+        private void clearButton(object sender, EventArgs e)
+        {
+            girlsNameBox.Text = "";
+            boysNameBox.Text = "";
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
+
+        private void exitButton(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
